1c0fed3e07f945b0f5982c9c8ae66ff7e8a55c9b
Guilherme Roberto dos Passos <guilhermerobertodospassos@gmail.com>
2023-11-23 16:30:22 -0300

    Starting the Navbar component

---
+++ meu_portfolio/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import styles from './Navbar.module.css'
+
+function Navbar(){
+    <nav className="navbar">
+        <span className="logo">{"<G/>"}</span>
+        <ul className="nav-list">
+            <li className="nav-item"><a>Sobre mim</a></li>
+            <li className="nav-item"><a>Projetos</a></li>
+        </ul>
+    </nav>
+}
+export default Navbar;
