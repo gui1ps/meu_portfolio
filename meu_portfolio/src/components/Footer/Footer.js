70727efc7cd84d22de73be6457627a775325b4ec
Guilherme Roberto dos Passos <guilhermerobertodospassos@gmail.com>
2024-03-25 16:30:02 -0300

    Adicionando funcionabilidade para o botão de download do meu currículo

--- meu_portfolio/src/components/Footer/Footer.js
+++ meu_portfolio/src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         fluid
         className="d-flex justify-content-sm-between justify-content-center flex-column flex-sm-row bg-dark text-center mt-5 py-4 px-md-5 px-sm-3 fs-6"
       >
-        <p className="m-0">Copyright © 2021.Todos os direitos reservados.</p>
+        <p className="m-0">Copyright © 2024.Todos os direitos reservados.</p>
         <span className="d-none d-sm-block">
           Powered by <b>{logo}</b>
         </span>
--- meu_portfolio/src/components/Presentation/Btn/Btn.js
+++ meu_portfolio/src/components/Presentation/Btn/Btn.js
@@ -1,9 +1,15 @@
 import styles from "./Btn.module.css";
-
+import { saveAs } from 'file-saver';
+import pdf from './cv.pdf'
 function Btn() {
+  const handleDownload=()=>{
+    const pdfPath=pdf;
+    const pdfFileName="Currículo  | Guilherme Roberto dos Passos.pdf";
+    saveAs(pdfPath,pdfFileName)
+  }
   return (
     <>
-      <button className={styles.Btn}>Download CV</button>
+      <button onClick={handleDownload} className={styles.Btn}>Download CV</button>
     </>
   );
 }
